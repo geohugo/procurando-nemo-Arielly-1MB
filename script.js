5f1a9a452d3130ac59d1d430bfb5e69a8c065ec9
Hugo Ribeiro Borges de Paula <101531832+geohugo@users.noreply.github.com>
2025-11-06 08:57:12 -0300

    Correção linhas 5 e 6

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 const avanca = document.querySelectorAll('.btn-proximo');
 
 avanca.forEach(button => {
-  button.addEventlistener('click', function () {
-    const atual = document.querySelector('.ativa');
-    const proximoPasso = 'passo-' + this.getAtribute('data-proximo');
+  button.addEventListener('click', function () {
+    const atual = document.querySelector('.ativo');
+    const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
 
     atual.classList.remove('ativo');
     document.getElementById(proximoPasso).classList.add('ativo');
