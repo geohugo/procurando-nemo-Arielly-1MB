61668b336c7c6d2320b2a1aea62eac30b50943b0
Hugo Ribeiro Borges de Paula <101531832+geohugo@users.noreply.github.com>
2025-11-06 08:49:05 -0300

    formatar html, css e js

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const avanca = document. querySelectorAll('.btn-proximo');
+const avanca = document.querySelectorAll('.btn-proximo');
 
 avanca.forEach(button => {
-    button.addEventlistener('click',function(){
-      const atual = document.querySelector('.ativa');
-      const proximoPasso = 'passo-' + this.getAtribute('data-proximo');
+  button.addEventlistener('click', function () {
+    const atual = document.querySelector('.ativa');
+    const proximoPasso = 'passo-' + this.getAtribute('data-proximo');
 
-       atual.classList.remove('ativo');
-       document.getElementById(proximoPasso).classList.add('ativo');
-    })
+    atual.classList.remove('ativo');
+    document.getElementById(proximoPasso).classList.add('ativo');
+  })
 })
